afd9ed9ac6f2039d902744d5d3f8acc2dbab11ee
juanda121102 <jancieta3@gmail.com>
2025-09-16 21:10:52 -0400

    cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\FileTaskRepository;
+use App\Repositories\TaskRepositoryInterface;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
+     
     public function register(): void
     {
         //
+        $this->app->bind(
+            TaskRepositoryInterface::class,
+            FileTaskRepository::class   
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
 }
